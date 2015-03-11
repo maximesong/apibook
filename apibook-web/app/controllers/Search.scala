25bf07eea6d1d76e9ff4d9697fd001911b13b4ac
Maxime Song <me@cppdo.com>
2015-03-11 15:47:54 +0800

    able to search from web

--- apibook-web/app/controllers/Search.scala
+++ apibook-web/app/controllers/Search.scala
@@ -1,13 +1,23 @@
 package controllers
 
 import com.cppdo.apibook.index.IndexManager
+import play.api.libs.json.{JsString, JsArray}
 import play.api.mvc._
+import play.api.libs.json.Json
+
 /**
  * Created by song on 3/11/15.
  */
 object Search extends Controller {
   def post = Action(parse.json) { implicit request =>
-    IndexManager
-    Ok(request.body)
+    val searchText = (request.body \ "searchText").as[String]
+    val resultEntries = IndexManager.search(searchText).map(document => {
+      document.get("name")
+    })
+    val result = Json.toJson(resultEntries)
+    Ok(Json.obj(
+      "request" -> request.body,
+      "result" -> result
+    ))
   }
 }
--- apibook-web/public/javascripts/apibook.js
+++ apibook-web/public/javascripts/apibook.js
@@ -1,12 +1,35 @@
 angular.module('apibookApp', [])
-    .controller('APIBookController', ['$scope', function($scope) {
+    .controller('APIBookController', ['$scope', '$http', function($scope, $http) {
         console.log("HI");
-        var postJson = function (url, data, success) {
-            $.ajax()
+        var postJson = function (url, data, success, error) {
+            $.ajax({
+                method: "POST",
+                contentType: "application/json",
+                url: url,
+                data: data,
+                success: success,
+                error: error,
+                dataType: "json"
+            });
         };
         $scope.search = function () {
+            var searchOptions = {
+                searchText: $scope.searchText
+            };
+            var response = $http.post("/api/search", searchOptions)
+            response.success(function(data) {
+                console.log(data);
+                $scope.items = data.result;
+                console.log($scope.items);
+            })
+            /*
             console.log("Search");
-            postJson("/api/search")
+            postJson("/api/search", JSON.stringify(searchOptions), function (data) {
+                console.log(data);
+                $scope.items = data.result;
+                $digest();
+            });
+            */
         };
     }]);
 
