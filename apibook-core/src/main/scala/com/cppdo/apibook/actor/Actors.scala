15fec4208746347a4e5f5bb5f704f919a6ddcaf8
Maxime Song <me@cppdo.com>
2015-03-20 11:04:12 +0800

    start to work with actor

---
+++ apibook-core/src/main/scala/com/cppdo/apibook/actor/Actors.scala
@@ -0,0 +1,12 @@
+package com.cppdo.apibook.actor
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+/**
+ * Created by song on 3/20/15.
+ */
+
+class TryActor extends Actor {
+  override def receive: Receive = ???
+}
