a8adbcf638af27f4ae28ec1955fb2bcbe6b22c2f
Maxime Song <me@cppdo.com>
2015-10-04 15:41:47 +0800

    add mongo to utility

--- utility/main.js
+++ utility/main.js
@@ -1,15 +1,27 @@
 var program = require('commander');
+var MongoClient = require('mongodb').MongoClient
+var mongoUrl = 'mongodb://localhost:27017/apibook'
 
 program
   .version('0.0.1')
   .option('-n, --number', 'number')
 
 program
-  .command('test <cmd>')
+  .command('test')
   .description('this command is for test')
   .action(function(cmd, options) {
-    console.log(options)
-    console.log('testing!')
+    console.log('running testing...');
+    read();
   })
 
+function read() {
+  MongoClient.connect(mongoUrl, function(err, db) {
+    var questions = db.collection("questions")
+    questions.find({}).toArray(function(err, data) {
+      console.log(JSON.stringify(data, null, 4));
+      db.close();
+    })
+    console.log(err);
+  })
+}
 program.parse(process.argv);
